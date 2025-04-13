47b5332478f1b8c0133ee4904d40b38bac75626f
michaelmccrae <mike@mccrae.ca>
2025-04-13 13:19:25 -0700

    No tracker again

---
+++ app/n/modelling-data/twitterpgmanimated.jsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <><blockquote class="twitter-tweet" data-media-max-width="560"><p lang="en" dir="ltr">Rhodium, palladium are riding high. Platinum, not so much <a href="https://t.co/gfB7p90V5S">https://t.co/gfB7p90V5S</a> via <a href="https://twitter.com/neil_c?ref_src=twsrc%5Etfw">@neil_C</a>, <a href="https://twitter.com/KitcoNewsNOW?ref_src=twsrc%5Etfw">@kitconewsnow</a> <a href="https://t.co/Lmpyr5zIBD">pic.twitter.com/Lmpyr5zIBD</a></p>&mdash; Michael McCrae (@michaelmccrae) <a href="https://twitter.com/michaelmccrae/status/1105903869744209920?ref_src=twsrc%5Etfw">March 13, 2019</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></>
+    )
+  }
