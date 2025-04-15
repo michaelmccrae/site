c2b0968fbd766d984cc1560be62c54fe9a3179a1
michaelmccrae <mike@mccrae.ca>
2025-04-15 16:14:24 -0700

    General updates, and moves to first person.

--- app/n/anchoring/randysyoutube.jsx
+++ app/n/anchoring/randysyoutube.jsx
@@ -1,5 +1,15 @@
 export default function Page() {
     return (
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/sNm9V_csm_I?si=iZ2OJZ9RBu2GRHyQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    )
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/sNm9V_csm_I?si=iZ2OJZ9RBu2GRHyQ"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    );
   }
+  
